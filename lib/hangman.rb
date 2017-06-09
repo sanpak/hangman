e598da57870079a25ccf1f25dff6ae19a639f8bb
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-09 00:04:14 -0400

    phase I

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,4 +6,14 @@ class HumanPlayer
 end
 
 class ComputerPlayer
+  attr_accessor :secret_word
+  def initialize(dictionary)
+    @secret_word = dictionary[rand(dictionary.length)]
+    p @secret_word
+    # @secret_word = contents[rand(contents.length)].chomp
+  end
+
+  def pick_secret_word
+    @secret_word.length
+  end
 end
