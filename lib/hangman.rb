caf5dd19665f5a442bd19e5db9c9fc3d46d17583
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-10 21:48:22 -0400

    phase ii

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,35 @@
 class Hangman
   attr_reader :guesser, :referee, :board
+  def initialize(players)
+    # puts "__________guesser________________"
+    @guesser =  players[:guesser]
+    @referee = players[:referee]
+    @board = board
+  end
+
+  def take_turn
+    puts "_________take_turn____________"
+    # puts "what's your guess?"
+    # player_guess = gets.chomp
+    @guesser.guess
+    setup
+    update_board
+    @guesser.handle_response
+  end
+
+  def setup
+    puts "__________setup_____________"
+    p @referee.pick_secret_word
+    p @guesser.register_secret_length(@referee.pick_secret_word)
+    p @board = Array.new(@referee.pick_secret_word)
+    p @referee.check_guess(@guesser.guess)
+  end
+
+  def update_board
+    puts "_______________update_board____________"
+    p @board
+  end
+
 end
 
 class HumanPlayer
@@ -16,4 +46,35 @@ class ComputerPlayer
   def pick_secret_word
     @secret_word.length
   end
+
+  def check_guess(letter)
+    result = []
+    @secret_word.chars.each_with_index do |secret_letter,idx|
+      result << idx if secret_letter == letter
+    end
+    result
+  end
+
+  def guess(board)
+    # puts "________letter__________"
+    p board[1]
+    #  player.register_secret_length(6)
+  end
+
+  def candidate_words
+    puts "_________________candidate_WORDS___________"
+    candidate_words = []
+    candidate_words << self.secret_word
+    p candidate_words
+  end
+
+  def register_secret_length(length)
+
+
+  end
+
+  def handle_response(letter,array)
+  end
+
+
 end
