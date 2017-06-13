bcd2ca2d0616c75257a7dd9e538fc6f765651e97
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-12 22:13:30 -0400

    redo hangman for practice

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,52 +1,23 @@
 class Hangman
   attr_reader :guesser, :referee, :board
   def initialize(players)
-    # puts "__________guesser________________"
-    @guesser =  players[:guesser]
-    @referee = players[:referee]
-    @board = board
   end
 
-  def take_turn
-
-    # puts "what's your guess?"
-    # player_guess = gets.chomp
-    @guesser.guess
-    setup
-    update_board
-    @guesser.handle_response
-  end
-
-  def setup
-    @referee.pick_secret_word
-    @guesser.register_secret_length(@referee.pick_secret_word)
-    @board = Array.new(@referee.pick_secret_word)
-    @referee.check_guess(@guesser.guess)
-  end
-
-  def update_board
-    @board
-  end
 
 end
 
 class HumanPlayer
-  attr_accessor :secret_word, :dictionary, :length, :candidate_words
-  def initialize(secret_word_length)
-    contents = File.readlines(dictionary.txt).chomp
-    contents.reject! { |word| word.length != secret_word_length }
-    @secret_word = contents[rand(contents.length)]
-  end
+
+
 
 
 end
 
 class ComputerPlayer
-  attr_accessor :secret_word, :dictionary, :candidate_words
+
   def initialize(dictionary)
     @dictionary = dictionary
-    @secret_word = dictionary[rand(dictionary.length)]
-    # @secret_word = contents[rand(contents.length)].chomp
+    @secret_word = @dictionary.sample
   end
 
   def pick_secret_word
@@ -54,48 +25,11 @@ class ComputerPlayer
   end
 
   def check_guess(letter)
-    result = []
-    @secret_word.chars.each_with_index do |secret_letter,idx|
-      result << idx if secret_letter == letter
+    found_letters_idx = []
+    @secret_word.chars.each_with_index do |el,idx|
+      found_letters_idx << idx if el == letter
     end
-    result
-  end
-
-  def check_word(word,letter)
-    pos_array = []
-    word.chars.each_with_index do |alphabet,idx|
-      pos_array << idx if alphabet == letter
-    end
-    return pos_array
-  end
-
-  def guess(board)
-    register_secret_length(@secret_word.length)
-    random_guess = ("a".."z").to_a[rand(("a".."z").to_a.length)]
-    # check_guess(random_guess)
-    # handle_response(random_guess,check_guess(random_guess))
-    letter_freq_hash = Hash.new(0)
-    candidate_words.each do |word|
-      word.chars.each do |letter|
-        letter_freq_hash[letter] += 1
-      end
-    end
-    puts "____________guess______________"
-    p board
-    board.each do |ch|
-      letter_freq_hash.delete(ch)
-    end
-      letter_freq_hash.sort_by { |k,v| v }[-1][0]
-  end
-
-  def register_secret_length(length)
-    @length = length
-    @candidate_words = @dictionary.select { |word| word.length == @length }
-  end
-
-  def handle_response(letter,idx)
-    candidate_words.select! { |word| check_word(word,letter) == idx }
-    candidate_words.reject! { |word| word.include?(letter) } if idx.empty?
+    found_letters_idx
   end
 
 
